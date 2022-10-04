328e1ae123ed1ed561b0f885d220d508632c8fed
Mujtaba Basheer <mujtaba.fleapo@gmail.com>
2022-10-05 02:08:19 +0530

    config changed 5

--- next.config.js
+++ next.config.js
@@ -1,14 +1,17 @@
 const { PHASE_PRODUCTION_BUILD } = require("next/constants");
 
-const isProd = phase === PHASE_PRODUCTION_BUILD && process.env.STAGING !== "1";
+module.exports = (phase, { defaultConfig }) => {
+  /**
+   * @type {import('next').NextConfig}
+   */
 
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
-  basePath: isProd ? "/trading-leagues-landing" : undefined,
-  assetPrefix: isProd ? "/trading-leagues-landing" : undefined,
-  env: {},
+  const isProd =
+    phase === PHASE_PRODUCTION_BUILD && process.env.STAGING !== "1";
+
+  const nextConfig = {
+    basePath: isProd ? "/trading-leagues-landing" : undefined,
+    assetPrefix: isProd ? "/trading-leagues-landing" : undefined,
+    env: {},
+  };
+  return nextConfig;
 };
-
-module.exports = nextConfig;
