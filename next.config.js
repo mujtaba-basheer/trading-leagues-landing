92717628ead5d8a7a1ab59248ee7b533c76d7d8b
Mujtaba Basheer <mujtaba.fleapo@gmail.com>
2022-10-05 01:15:13 +0530

    changed configs

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
+const isProd = process.env.NODE_ENV === "production";
+
 /**
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
   /* config options here */
-  basePath: "",
+  assetPrefix: isProd
+    ? "https://mujtaba-basheer.github.io/trading-leagues-landing"
+    : undefined,
 };
 
 module.exports = nextConfig;
